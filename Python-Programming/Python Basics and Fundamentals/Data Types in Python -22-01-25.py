62d580b025708f6b836eaaba7ccc38ecabaa5367
Avijit Roy <avijitroy926avijit@gmail.com>
2025-04-26 02:03:08 +0530

    Add files via upload

---
+++ Python-Programming/Python Basics and Fundamentals/Data Types in Python -22-01-25.py
@@ -0,0 +1,11 @@
+x = 10
+print("Type of x: ", type(x))
+
+y = 15.6
+print("\nType of y: ", type(y))
+
+z = 1 + 2j
+print("\nType of z: ", type(z))
+
+print('Data Type of \'True\':', type(True))
+print("Data Type of \'False\':", type(False))
